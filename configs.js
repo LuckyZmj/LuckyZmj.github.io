50384f20a95146c2ecf7758aa72b68517a49e191
LuckyZmj <50950407+LuckyZmj@users.noreply.github.com>
2021-06-13 00:09:08 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -3,7 +3,7 @@ YL.static = {
   softwareName: 'Luckysec', //网站名。请在此处填写您自己的网站名，如王小明的博客。
   version: "1.0.0", // 网站版本号
   iconBtnStart: 'html5', //主图标
-  author: 'Luckey',//作者
+  author: 'Luckysec',//作者
   contactInformation: 'QQ：16463223',//联系方式
   officialWebsite: 'http://www.luckysec.cn',//软件官网
   welcome: '本网站UI由 YLUI 强力驱动\n更多信息：//ylui.yuri2.cn',//加载完毕控制台提示信息
